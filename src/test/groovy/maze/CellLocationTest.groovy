2267809e651ede8b46be1e492c535c3caaf8d1fd
whamondg <gordon.whamond@googlemail.com>
2017-03-13 19:47:42 +0000

    remove duplicate tests

--- src/test/groovy/maze/CellLocationTest.groovy
+++ src/test/groovy/maze/CellLocationTest.groovy
@@ -41,32 +41,5 @@ class CellLocationTest extends Specification {
         2   | 1
         2   | 2
     }
-
-    def "cell links are initially empty"() {
-        setup:
-        def cell = new Cell( 1, 1 )
-
-        when:
-        def result = cell.links
-
-        then:
-        result.size() == 0
-    }
-
-    def "cell link"() {
-        setup:
-        def firstCell = new Cell( firstCellRow, firstCellColumn )
-        def secondCell = new Cell( secondCellRow, secondCellColumn )
-
-        when:
-        firstCell.link( secondCell )
-
-        then:
-        assert firstCell.linkedTo( secondCell )
-
-        where:
-        firstCellRow | firstCellColumn | secondCellRow | secondCellColumn
-        1            | 2               | 2             | 1
-
-    }
+    
 }
