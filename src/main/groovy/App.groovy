757d95f425839282baa2c78e97cde8778ba65846
whamondg <gordon.whamond@googlemail.com>
2017-03-19 12:07:16 +0000

    add binary tree algorithm to App

--- src/main/groovy/App.groovy
+++ src/main/groovy/App.groovy
@@ -1,11 +1,15 @@
 import maze.Grid
+import maze.algorithm.BinaryTreeAlgorithm
 
 int row = args[0] as int
 int col = args[1] as int
 
 println()
 println "Creating Maze with dimensions $row $col"
+println()
+
 def grid = new Grid( row, col )
 
-println()
+new BinaryTreeAlgorithm().on( grid )
+
 println grid
