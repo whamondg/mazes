27ee89d724b72de9480444df3416764cc1f9abbc
whamondg <gordon.whamond@googlemail.com>
2018-01-24 22:44:09 +0000

    convert start to be simple position

--- src/main/groovy/App.groovy
+++ src/main/groovy/App.groovy
@@ -14,18 +14,17 @@ Creating Maze:
     Dimensions = ${ grid.dimensions() }
 """
 
-//new BinaryTreeAlgorithm().on( grid )
-//println "Binary Tree maze"
-//println grid
-//
-//Grid grid2 = new Grid( row, col )
-//new SidewinderAlgorithm().on( grid2 )
-//println "Sidewinder maze"
-//println grid2
+new BinaryTreeAlgorithm().on( grid )
+println "Binary Tree maze"
+println grid
 
+Grid grid2 = new Grid( row, col )
+new SidewinderAlgorithm().on( grid2 )
+println "Sidewinder maze"
+println grid2
+
+println "Distance grid solved with Dijkstra's Algorithm"
 DistanceGrid distanceGrid = new DistanceGrid(row,col)
 new SidewinderAlgorithm(  ).on(distanceGrid)
-println distanceGrid
-
-distanceGrid.start(distanceGrid.cell(1,1))
+distanceGrid.start(1,1)
 println distanceGrid
--- src/main/groovy/maze/grid/DistanceGrid.groovy
+++ src/main/groovy/maze/grid/DistanceGrid.groovy
@@ -7,12 +7,11 @@ class DistanceGrid extends Grid {
         super( rows, columns )
     }
 
-    void start( Cell cell ) {
-        distances = cell.distances()
+    void start( int row, int column ) {
+        distances = cell(row, column).distances()
     }
 
     String cellContent( Cell cell ) {
         (distances && distances[cell] != null) ? distances[cell].toString(  ) : super.cellContent()
     }
-
 }
