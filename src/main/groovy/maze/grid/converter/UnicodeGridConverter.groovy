80cc6c0cff6fb6cc7300bd99fab851bb37b72815
Gordon Whamond <gordon.whamond@sky.uk>
2018-01-29 21:39:24 +0000

    format code

--- src/main/groovy/maze/grid/converter/UnicodeGridConverter.groovy
+++ src/main/groovy/maze/grid/converter/UnicodeGridConverter.groovy
@@ -1,10 +1,9 @@
 package maze.grid.converter
 
-import maze.grid.Cell
 import maze.grid.Grid
 
 class UnicodeGridConverter implements GridConverter<String> {
-    static final String LINE_END = System.getProperty( "line.separator" )
+    static final String LINE_END = System.getProperty("line.separator")
     def parts = [
             TOP_LEFT       : '┏',
             TOP_JOINT      : '┳',
@@ -21,38 +20,37 @@ class UnicodeGridConverter implements GridConverter<String> {
             HORIZONTAL_LINK: '    '
     ]
 
-    String convertGrid( Grid grid ) {
+    String convertGrid(Grid grid) {
         def buffer = new StringBuffer()
         buffer << parts.TOP_LEFT
         (grid.columns - 1).times {
-            buffer <<  parts.HORIZONTAL_EDGE << parts.TOP_JOINT
+            buffer << parts.HORIZONTAL_EDGE << parts.TOP_JOINT
         }
-        buffer <<  parts.HORIZONTAL_EDGE << parts.TOP_RIGHT << LINE_END
+        buffer << parts.HORIZONTAL_EDGE << parts.TOP_RIGHT << LINE_END
 
         grid.gridRows.each { row ->
             def middleRow = [parts.VERTICAL_EDGE]
             def bottomRow = [] << (row.lastRow ? parts.BOTTOM_LEFT : parts.LEFT_JOINT)
 
             row.each { cell ->
-                middleRow << " ${grid.cellContent( cell ).padRight(3)}"
-                middleRow << (cell?.linkedTo( cell.east ) ? parts.VERTICAL_LINK : parts.VERTICAL_EDGE)
+                middleRow << " ${grid.cellContent(cell).padRight(3)}"
+                middleRow << (cell?.linkedTo(cell.east) ? parts.VERTICAL_LINK : parts.VERTICAL_EDGE)
 
-                bottomRow << (cell?.linkedTo( cell.south ) ? parts.HORIZONTAL_LINK : parts.HORIZONTAL_EDGE)
+                bottomRow << (cell?.linkedTo(cell.south) ? parts.HORIZONTAL_LINK : parts.HORIZONTAL_EDGE)
 
-                if ( row.lastRow ) {
+                if (row.lastRow) {
                     bottomRow << (cell.lastCell ? parts.BOTTOM_RIGHT : parts.BOTTOM_JOINT)
                 } else {
                     bottomRow << (cell.lastCell ? parts.RIGHT_JOINT : parts.CORNER)
                 }
             }
 
-            middleRow.each { buffer << it}
+            middleRow.each { buffer << it }
             buffer << LINE_END
-            bottomRow.each { buffer << it}
+            bottomRow.each { buffer << it }
             buffer << LINE_END
         }
 
         buffer.toString()
     }
-
 }
