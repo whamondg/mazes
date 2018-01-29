46b5edb7f565ff3147d6cf4e810d71bd0f8c91e4
Gordon Whamond <gordon.whamond@sky.uk>
2018-01-29 12:38:29 +0000

    give verbose options double dashes

--- src/main/groovy/maze/cli/Settings.groovy
+++ src/main/groovy/maze/cli/Settings.groovy
@@ -10,19 +10,19 @@ class Settings {
     @Parameter(names = ["-h", "--help"], help = true)
     private boolean help
 
-    @Parameter(names = ["-r", "-rows"], description = "Number of rows to add to the grid")
+    @Parameter(names = ["-r", "--rows"], description = "Number of rows to add to the grid")
     Integer rows = random.nextInt(maxRandomGridSize) + minRandomDridSize
 
-    @Parameter(names = ["-c", "-cols"], description = "Number of columns to add to the grid")
+    @Parameter(names = ["-c", "--cols"], description = "Number of columns to add to the grid")
     Integer cols = random.nextInt(maxRandomGridSize) + minRandomDridSize
 
-    @Parameter(names= ["-a", "-algorithm"], description = "The algorithm to use when drawing the maze.  If absent a gallery of mazes will be produced using each algorithm in turn")
+    @Parameter(names= ["-a", "--algorithm"], description = "The algorithm to use when drawing the maze.  If absent a gallery of mazes will be produced using each algorithm in turn")
     String algorithm
 
-    @Parameter(names = ["-s", "-start"], description = "Cell position to use as the start of the maze", splitter = CommaSplitter.class)
+    @Parameter(names = ["-s", "--start"], description = "Cell position to use as the start of the maze", splitter = CommaSplitter.class)
     List<Integer> start
 
-    @Parameter(names = ["-e", "-end"], description = "Cell position to use as the end of the maze", splitter = CommaSplitter.class)
+    @Parameter(names = ["-e", "--end"], description = "Cell position to use as the end of the maze", splitter = CommaSplitter.class)
     List<Integer> end
 
 }
