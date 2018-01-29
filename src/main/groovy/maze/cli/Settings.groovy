8f35577b8a47bd15e627c5a1b2b9ddc2de314209
Gordon Whamond <gordon.whamond@sky.uk>
2018-01-29 13:05:06 +0000

    order parameters in usage statement

--- src/main/groovy/maze/cli/Settings.groovy
+++ src/main/groovy/maze/cli/Settings.groovy
@@ -5,21 +5,21 @@ import com.beust.jcommander.Parameter
 class Settings {
     int defaultSize = 10
 
-    @Parameter(names = ["-h", "--help"], help = true)
+    @Parameter(names = ["-h", "--help"], help = true, order = 0)
     private boolean help
 
-    @Parameter(names = ["-r", "--rows"], description = "Number of rows to add to the grid")
-    Integer rows = defaultSize
-
-    @Parameter(names = ["-c", "--cols"], description = "Number of columns to add to the grid")
-    Integer cols = defaultSize
-
-    @Parameter(names= ["-a", "--algorithm"], description = "The algorithm to use when drawing the maze.  If absent a gallery of mazes will be produced using each algorithm in turn")
+    @Parameter(names= ["-a", "--algorithm"], order = 1, description = "The algorithm to use when drawing the maze.  If absent a gallery of mazes will be produced using each algorithm in turn")
     String algorithm
 
-    @Parameter(names = ["-s", "--start"], description = "Cell position to use as the start of the maze", splitter = CommaSplitter.class)
+    @Parameter(names = ["-r", "--rows"], order = 2, description = "Number of rows to add to the grid")
+    Integer rows = defaultSize
+
+    @Parameter(names = ["-c", "--cols"], order = 3, description = "Number of columns to add to the grid")
+    Integer cols = defaultSize
+
+    @Parameter(names = ["-s", "--start"], order = 4, description = "Cell position to use as the start of the maze", splitter = CommaSplitter.class)
     List<Integer> start
 
-    @Parameter(names = ["-e", "--end"], description = "Cell position to use as the end of the maze", splitter = CommaSplitter.class)
+    @Parameter(names = ["-e", "--end"], order = 5, description = "Cell position to use as the end of the maze", splitter = CommaSplitter.class)
     List<Integer> end
 }
