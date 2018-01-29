55916d79a75b8bedbae4ba16579a679b9dfebad1
Gordon Whamond <gordon.whamond@sky.uk>
2018-01-29 12:43:01 +0000

    set default size to be 10x10

--- src/main/groovy/maze/cli/Settings.groovy
+++ src/main/groovy/maze/cli/Settings.groovy
@@ -3,18 +3,16 @@ package maze.cli
 import com.beust.jcommander.Parameter
 
 class Settings {
-    Random random = new Random()
-    int maxRandomGridSize = 15
-    int minRandomDridSize = 2
+    int defaultSize = 10
 
     @Parameter(names = ["-h", "--help"], help = true)
     private boolean help
 
     @Parameter(names = ["-r", "--rows"], description = "Number of rows to add to the grid")
-    Integer rows = random.nextInt(maxRandomGridSize) + minRandomDridSize
+    Integer rows = defaultSize
 
     @Parameter(names = ["-c", "--cols"], description = "Number of columns to add to the grid")
-    Integer cols = random.nextInt(maxRandomGridSize) + minRandomDridSize
+    Integer cols = defaultSize
 
     @Parameter(names= ["-a", "--algorithm"], description = "The algorithm to use when drawing the maze.  If absent a gallery of mazes will be produced using each algorithm in turn")
     String algorithm
@@ -24,6 +22,4 @@ class Settings {
 
     @Parameter(names = ["-e", "--end"], description = "Cell position to use as the end of the maze", splitter = CommaSplitter.class)
     List<Integer> end
-
 }
-
